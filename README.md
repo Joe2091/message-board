91c956a192a074665aaad2b86d7873d0feadf637
Joe <x21476132@student.ncirl.ie>
2025-04-10 20:52:35 +0100

    Project Setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Message Board Web Application
+
+A simple web application using Node.js, Express, and SQLite3. This application has two branches the secure (main) branch and the `insecure` branch which will introduce vulnerabilities such as SQL Injection, XSS Reflected Dom based and stored and Sensitive Data Exposure The `insecure` branch will contain an intentionally vulnerable version for security testing and learning.
+
+# How to Run the Application
+
+Clone this repository:
+
+In Terminal/cmd
+
+git clone https://github.com/Joe2091/message-board.git
+cd secure-message-board
+npm i
+node index.js
+running on : http://localhost:3000
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const app = express();
+const db = new sqlite3.Database('db.sqlite3');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Create table
+db.run('CREATE TABLE IF NOT EXISTS messages (name TEXT, message TEXT)');
+
+app.get('/', (req, res) => {
+  db.all('SELECT * FROM messages', [], (err, rows) => {
+    if (err) {
+      return res.status(500).send('Database error');
+    }
+    res.render('index', { messages: rows });
+  });
+});
+
+app.post('/message', (req, res) => {
+  const { name, message } = req.body;
+  db.run('INSERT INTO messages (name, message) VALUES (?, ?)', [name, message], (err) => {
+    if (err) {
+      return res.status(500).send('Failed to post message');
+    }
+    res.redirect('/');
+  });
+});
+
+app.listen(3000, () => {
+  console.log('message board application running at http://localhost:3000');
+});
